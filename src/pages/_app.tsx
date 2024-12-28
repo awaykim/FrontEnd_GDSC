918eef11fd317334717e8ae14166182fe57ff217
awaykim <wonj010101@gmail.com>
2024-12-29 03:49:20 +0900

    context api (_app)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,26 @@
 import Header from "@/components/Header";
 import "@/styles/globals.css";
 import { AppProps } from "next/app";
+import { createContext, useContext, useState } from "react";
+
+export const userContext = createContext({
+  name: "",
+  setName: (name: string) => {},
+});
 
 export default function App({ Component, pageProps }: AppProps) {
+
+  const [name, setName] = useState("");
+
   return (
-    <>
-      {/* App 파일에 컴포넌트 넣으면 모든 페이지에 자동으로 배치됨*/}
-      <Header />
-      <Component {...pageProps} />
-    </>
+    <userContext.Provider value={{ name, setName }}>
+          <>
+              {/* App 파일에 컴포넌트 넣으면 모든 페이지에 자동으로 배치됨*/}
+              <Header />
+              <Component {...pageProps} />
+          </>
+      </userContext.Provider>
   );
 }
+
+export const useUserName = () => useContext(userContext);
